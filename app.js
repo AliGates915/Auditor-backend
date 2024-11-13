05d71cda251075c69315d6f4e045d3cb1d38de0c
AliGates915 <hacktech877@gmail.com>
2024-11-13 19:58:21 +0500

    ok

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ const app = express();
 
 const clientURL = 'https://auditor-frontend.vercel.app';
 
-app.use(
-  cors({
-  origin: clientURL,
-  })
-);
+// app.use(
+//   cors({
+//   origin: clientURL,
+//   })
+// );
 
 app.use((req, res, next) => {
   res.header(
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
-  // res.setHeader( "Access-Control-Allow-Origin", "https://auditor-frontend.vercel.app" );
+  res.setHeader( "Access-Control-Allow-Origin", "https://auditor-frontend.vercel.app" );
   
   console.log("Request received:", req.method, req.url);
   
