511dc9eb87b6432ac6bd081dcbded5687fcca4d0
AliGates915 <hacktech877@gmail.com>
2024-11-14 02:00:42 +0500

    new Data

--- app.js
+++ app.js
@@ -8,28 +8,28 @@ const adminRoutes = require('./routes/adminRoutes');
 
 // Load environment variables
 dotenv.config();
+const app = express();
 
-// app.use(
-//   cors({
-//     origin: ["https://localhost:5173", "https://auditor-frontend.vercel.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(cors({
+  origin: ["https://localhost:5173", "https://auditor-frontend.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
-app.use((req, res, next) => {
-  res.header(
-  'Access-Control-Allow-Origin',
-  "https://auditor-frontend.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
+// app.use((req, res, next) => {
+//   res.header(
+//   'Access-Control-Allow-Origin',
+//   "https://auditor-frontend.vercel.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
   
-  console.log("Request received:", req.method, req.url);
+//   console.log("Request received:", req.method, req.url);
   
-  next();
-  });
+//   next();
+//   });
+
 
 // Middleware to parse JSON
 app.use(express.json());
