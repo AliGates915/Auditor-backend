1ca6a2d6c618988f4342527b9de5d4fbf8256123
AliGates915 <hacktech877@gmail.com>
2024-11-13 11:01:42 +0500

    new

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ mongoose
   });
 
 // Start server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 
