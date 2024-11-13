321de2bdb81fad34c1e2a2f4b705147d1711824f
AliGates915 <hacktech877@gmail.com>
2024-11-14 00:45:59 +0500

    ok

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: ["https://localhost:5173", "https://auditor-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    credentials: false,
   })
 );
 
