af11d4e9752594b9575bd0e765e751f30a668177
AliGates915 <hacktech877@gmail.com>
2024-11-13 19:55:43 +0500

    new

--- app.js
+++ app.js
@@ -13,13 +13,34 @@ dotenv.config();
 const app = express();
 
 // Enable CORS for all requests
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://auditor-frontend.vercel.app', 'https://cert.lpgexpress.com.pk'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, // Allow cookies and auth tokens
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'https://auditor-frontend.vercel.app', 'https://cert.lpgexpress.com.pk'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true, // Allow cookies and auth tokens
+// }));
 
+const clientURL = 'https://auditor-frontend.vercel.app';
 
+app.use(
+  cors({
+  origin: clientURL,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header(
+  'Access-Control-Allow-Origin',
+  "clientURL"
+  );
+  res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  // res.setHeader( "Access-Control-Allow-Origin", "https://auditor-frontend.vercel.app" );
+  
+  console.log("Request received:", req.method, req.url);
+  
+  next();
+  });
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -28,10 +49,7 @@ app.get('/', (req, res) => {
   res.send('Server running!');
 });
 
-app.use((req, res, next) => {
-  console.log('Request origin:', req.headers.origin);
-  next();
-});
+
 
 
 // Routes
