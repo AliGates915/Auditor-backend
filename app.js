1accaf4321c56804cf56f6744cb7bb4d3b2aa769
AliGates915 <hacktech877@gmail.com>
2024-11-14 11:03:00 +0500

    oooo

--- app.js
+++ app.js
@@ -4,34 +4,33 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-
+const app = express();
 
 // Load environment variables
 dotenv.config();
-const app = express();
+
 
 app.use(cors({
-  origin: ["https://localhost:5173", "https://auditor-frontend.vercel.app"],
+  origin: ["https://auditor-frontend.vercel.app"], // Allow only your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  credentials: true 
 }));
 
-// app.use((req, res, next) => {
+app.use((req, res, next) => {
 //   res.header(
 //   'Access-Control-Allow-Origin',
 //   "https://auditor-frontend.vercel.app"
 //   );
 //   res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
   
 //   console.log("Request received:", req.method, req.url);
   
-//   next();
-//   });
+  next();
+  });
 
 
-// Middleware to parse JSON
 app.use(express.json());
 
 app.get('/', (req, res) => {
