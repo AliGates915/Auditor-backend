dcbfa84a44dc9836971a6eeb3c7bd5ca00e0bb2d
AliGates915 <hacktech877@gmail.com>
2024-11-14 14:51:39 +0500

    new update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ dotenv.config();
 app.use((req, res, next) => {
   res.header(
   'Access-Control-Allow-Origin',
-  "https://auditor-frontend.vercel.app/"
+  "https://auditor-frontend.vercel.app/matloob"
   );
   res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
